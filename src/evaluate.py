b0f52992525bebe96c2fef630f4ff1efecd201ff
Tianran Liu <tianranl@uw.edu>
2022-05-22 18:11:48 -0700

    Add evaluation script for bleu and wer

---
+++ src/evaluate.py
@@ -0,0 +1,52 @@
+import nltk
+import os
+from jiwer import wer, cer
+
+
+def read_hypo_bleu(path):
+    with open(path, 'r') as f:
+        output = [line.strip().split(' ') for line in f.readlines()]
+    return output
+
+
+def read_ref_bleu(path):
+    with open(path, 'r') as f:
+        output = [[line.strip().split(' ')] for line in f.readlines()]
+    return output
+
+
+def cal_bleu(ref_path, hypo_path):
+    refs = read_ref_bleu(ref_path)
+    hypos = read_hypo_bleu(hypo_path)
+    bleu_score = nltk.translate.bleu_score.corpus_bleu(refs, hypos)
+    return bleu_score
+
+
+def read_file_wer(path):
+    with open(path, 'r') as f:
+        output = [line.strip() for line in f.readlines()]
+    return output
+
+
+def cal_wer_cer(ref_path, hypo_path):
+    refs = read_file_wer(ref_path)
+    hypos = read_file_wer(hypo_path)
+    wer_score = wer(refs, hypos)
+    cer_score = cer(refs, hypos)
+    return wer_score, cer_score
+    
+
+if __name__ == '__main__':
+    output_dir = 'outputs/twostage/'
+    ref_path = os.path.join(output_dir, 'gold_eng.txt')
+    hypo_path = os.path.join(output_dir, 'pred_eng_with_gold_swa.txt')
+    bleu_score = cal_bleu(ref_path, hypo_path)
+    print(bleu_score)
+    hypo_path = os.path.join(output_dir, 'pred_eng.txt')
+    bleu_score = cal_bleu(ref_path, hypo_path)
+    print(bleu_score)
+    ref_path = os.path.join(output_dir, 'gold_swa.txt')
+    hypo_path = os.path.join(output_dir, 'pred_swa.txt')
+    wer_score, cer_score = cal_wer_cer(ref_path, hypo_path)
+    print(wer_score)
+    print(cer_score)
