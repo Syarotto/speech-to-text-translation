2efb74d142eb35d05bc2d858a23ea4d11db50be2
Tianran Liu <tianranl@uw.edu>
2022-06-05 00:11:33 -0700

    Use default sampling rate

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,11 @@
-from swa_asr import swaASR
-from swa_nmt import NMT
 from dataset import iwsltDataset
 import os
 from tqdm import tqdm
 
 
-def evaluate():
+def evaluate_hf_pipeline():
+    from hf_asr import swaASR
+    from hf_nmt import NMT
     swa_asr = swaASR()
     swa_nmt = NMT(src_lang="sw", tar_lang="en")
     dataset = iwsltDataset(split='valid')
@@ -31,5 +31,39 @@ def evaluate():
             f.write(sent + '\n')
 
 
+def evaluate_googleASR():
+    from google_asr import transcribe_file
+    dataset = iwsltDataset(split='valid')
+    save_dir = 'outputs/GoogleCloud'
+    idx = -1
+    for file_path, gold_swa, gold_eng in tqdm(dataset, miniters=1):
+        idx += 1
+        if idx <= 169:
+            continue
+        try:
+            pred_swa = transcribe_file(file_path)
+        except:
+            pred_swa = ''
+        with open(os.path.join(save_dir, 'pred_swa.txt'), 'a') as f:
+                f.write(pred_swa + '\n')
+
+
+def evaluate_MT():
+    from hf_nmt import NMT
+    swa_nmt = NMT(src_lang="sw", tar_lang="en")
+    pred_eng_results = []
+    save_dir = 'outputs/GoogleCloud'
+
+    with open(os.path.join(save_dir, 'pred_swa.txt'), 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            pred_eng = swa_nmt.predict(line)
+            pred_eng_results.append(pred_eng)
+    
+    with open(os.path.join(save_dir, 'pred_eng.txt'), 'w') as f:
+        for sent in pred_eng_results:
+            f.write(sent + '\n')
+
+
 if __name__ == '__main__':
-    evaluate()
+    evaluate_MT()
